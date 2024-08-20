6fc41164bc1b37c75705a30583fd592d103cc82c
Richard Goldsmith <rgoldsmith727@gmail.com>
2024-08-20 01:11:44 -0400

    feat: func to play a single round

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let humanScore = 0
 let computerScore = 0
 
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors']
+  const choices = ['Rock', 'Paper', 'Scissors']
   const rand = Math.floor(Math.random() * 3)
   return choices[rand]
 }
@@ -10,3 +10,27 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt('Choose Rock, Paper, or Scissors')
 }
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+
+  if (humanChoice === computerChoice) {
+    return `It's a tie! Play again.`
+  }
+
+  if ((humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+    (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+    (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
+    humanScore += 1
+    return `You win! ${humanChoice} beats ${computerChoice}`
+  }
+  
+  computerScore += 1
+  return `You lose! ${computerChoice} beats ${humanChoice}`
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+console.log(playRound(humanSelection, computerSelection))
+
