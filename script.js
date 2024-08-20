0d84b6f787573a4acc656d407a0e725a4347e86c
Richard Goldsmith <rgoldsmith727@gmail.com>
2024-08-20 00:47:53 -0400

    feat: getComputerChoice func

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+/* let humanScore = 0
+let computerScore = 0 */
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors']
+  const rand = Math.floor(Math.random() * 3)
+  return choices[rand]
+}
+
+/* function getHumanChoice() {
+  return prompt('Choose Rock, Paper, or Scissors')
+} */
