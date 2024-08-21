c44dfbb1b2052b65d5353f541244a4359749291e
Richard Goldsmith <rgoldsmith727@gmail.com>
2024-08-21 18:04:11 -0400

    fix: remove event at winner

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 let humanScore = 0
 let computerScore = 0
 
-function getComputerChoice() {
+const getComputerChoice = () => {
   const choices = ['Rock', 'Paper', 'Scissors']
   const rand = Math.floor(Math.random() * 3)
   return choices[rand]
 }
 
+const isWinner = () => {
+  if (humanScore === 5 || computerScore === 5) {
+    if (humanScore > computerScore) {
+      console.log(`You Win!`)
+    } else {
+      console.log('You lose!')
+    }
+    btnContainer.removeEventListener('click', activateRound)
+  }
+}
+
 const activateRound = event => {
   if (humanScore < 5 && computerScore < 5) {
     playRound(event.target.id)
-    console.log(`Player: ${humanScore} Computer: ${computerScore}`)
-  } else {
-    humanScore > computerScore 
-    ? console.log(`You Win!`) : console.log(`You lose!`)
-    btnContainer.removeEventListener('click', activateRound)
-  }
+ }
 }
 
 const playRound = (humanChoice) => {
@@ -31,11 +37,17 @@ const playRound = (humanChoice) => {
     (humanChoice === 'Paper' && computerChoice === 'Rock') ||
     (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
     console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-    return humanScore += 1
+    humanScore += 1
+    console.log(`Player: ${humanScore} Computer: ${computerScore}`)
+    isWinner()
+    return
   }
   
   console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-  return computerScore += 1
+  computerScore += 1
+  console.log(`Player: ${humanScore} Computer: ${computerScore}`)
+  isWinner()
+  return
 
 }
 
